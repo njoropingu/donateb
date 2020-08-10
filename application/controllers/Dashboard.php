95e6082ce0ea59f23b4f78a3485d416f82936f6f
Benard <>
2020-08-10 18:28:52 +0300

    Admin section for viewing user details regarding donated amounts

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,17 +8,18 @@ class Dashboard extends CI_Controller {
         parent::__construct();
         $this->load->model('donor_model');
         $this->load->library('Authorization_Token');
-        if(!$this->session->userdata('token')){
-            redirect('login'); 
+        if (!$this->session->userdata('token')) {
+            redirect('login');
         }
 //       $is_valid_token= $this->authorization_token->validateToken();
 //       var_dump($is_valid_token);
     }
 
     function index() {
-          
-       
+
+
         $this->load->view('header');
+
         $data['parameter'] = $this->donor_model->getParameters();
         $data['donors'] = $this->donor_model->getAllDonors();
         $this->load->view('dashboard', $data);
@@ -29,24 +30,34 @@ class Dashboard extends CI_Controller {
         redirect('login');
     }
 
-    function update($id) {
+    function updated() {
+        $arr = array(
+            'conkey' => 'conkey',
+            'secret' => 'secret'
+        );
+      
+
+
+//        curl_close($ch);
+//        $this->donor_model->update($post);
+    }
+
+    function update($id){
         $post = array('conkey' => $this->input->post('conkey'),
-            'secret' => $this->input->post('secret'));
-        $data = $this->donor_model->update($id, $post);
-        if ($data) {
-            $this->updateDonate();
-        }
-        redirect('dashboard');
+        'secret' => $this->input->post('secret'));
+        $this->donor_model->update($id, $post);
+        redirect('testSend/my_test');
+      
     }
-
-    function updateDonate($conkey, $secret) {
-        $this->donor_model->updateParam($conkey, $secret);
-    }
-
     function edit($id) {
         $data['param'] = $this->donor_model->getById($id);
         $this->load->view('header', $data);
         $this->load->view('edit_parameters', $data);
     }
+
+//    function updateParam() {
+//        $data = $this->donor_model->updateParam();
+//     
+//    }
 
 }
